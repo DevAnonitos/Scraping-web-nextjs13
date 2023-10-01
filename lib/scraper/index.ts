49911c7a147a7a3febeedda120392013ea0692e2
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-10-01 21:02:39 +0700

    update:Add->ScrapingFuncSetingUp

--- lib/scraper/index.ts
+++ lib/scraper/index.ts
@@ -4,8 +4,34 @@ import axios from "axios";
 import * as cheerio from "cheerio";
 
 export async function scrapeAmazonProduct(url: string) {
+
+    if(!url) {
+        return;
+    }
+
+    const username = String(process.env.BRIGHT_DATA_USERNAME);
+    const password = String(process.env.BRIGHT_DATA_PASSWORD);
+
+    const port = 22225;
+
+    const session_id = (100000 * Math.random() | 0);
+
+    const options = {
+        auth: {
+            username: `${username}-session-${session_id}`,
+            password,
+        },
+        host: 'brd.superproxy.io',
+        port,
+        rejectUnauthorized: false,
+    };
+
     try {
-        
+        const response = await axios.get(url, options);
+
+        console.log(response.data);
+
+
     } catch (error: any) {
         console.log(error);
     }
