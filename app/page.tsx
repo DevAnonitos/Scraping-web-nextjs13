061ee124df548e6e6b213eed7dd632c227f20116
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-10-01 09:00:30 +0700

    update:Add->Assets-Folder

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
+import React from 'react';
 
-export default function Home() {
+const Home = () => {
   return (
-    <main>
-      HelloPage
-    </main>
+    <div>Home</div>
   );
 };
+
+export default Home;
