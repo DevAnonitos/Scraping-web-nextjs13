b3b6c243c3b5b1978a3bb7e80cf6d7296e5e7ca9
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-10-01 09:42:40 +0700

    update:Add->Navbar-components

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,43 @@
 import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+
+export const navIcons = [
+    { src: '/assets/icons/search.svg', alt: 'search', },
+    { src: '/assets/icons/black-heart.svg', alt: 'heart', },
+    { src: '/assets/icons/user.svg', alt: 'user', },
+];
 
 const Navbar = () => {
     return (
-        <div>Navbar</div>
+        <header className='w-full'>
+            <nav className='nav'>
+                <Link href="/" className='flex items-center gap-1'>
+                    <Image
+                        src="/assets/icons/logo.svg"
+                        width={27}
+                        height={27}
+                        alt='Logo'
+                    />
+                    <p className="nav-logo">
+                        Price<span className='text-primary'>Tracker</span>
+                    </p>
+                </Link>
+
+                <div className='flex items-center gap-5'>
+                    {navIcons.map((icon) => (
+                        <Image 
+                            key={icon.alt}
+                            src={icon.src}
+                            alt={icon.alt}
+                            width={28}
+                            height={28}
+                            className="object-contain cursor-pointer"
+                        />
+                    ))}
+                </div>
+            </nav>
+        </header>
     );
 };
 
