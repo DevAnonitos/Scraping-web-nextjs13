6be8497eb92645c648638ebd0dc23fab837b2659
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-10-01 10:44:43 +0700

    update:Add->HomePage

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,51 @@
 import React from 'react';
+import { 
+  HeroCarousel, 
+  SearchBar, 
+  ProductCard 
+} from '@/components';
+import Image from 'next/image';
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+      <section className='px-6 md:px-20 py-24'>
+        <div className='flex max-xl:flex-col gap-16'>
+          <div className='flex flex-col justify-center'> 
+            <p className='small-text'>
+              Smart Shopping Starts Here:
+              <Image 
+                src="/assets/icons/arrow-right.svg"
+                alt="arrow-right"
+                width={16}
+                height={16}
+              />
+            </p>
+
+            <h1 className='head-text'>
+              Unleash the Power of
+              <span className="text-primary"> Tracker</span>
+            </h1>
+
+            <p className="mt-6">
+              Powerful, self-serve product and growth analytics to help you convert, engage, and retain more.
+            </p>
+
+            <SearchBar />
+          </div>
+        </div>
+      </section>
+
+      <section className='trending-section'>
+        <h2 className="section-text">
+          Trending
+        </h2>
+
+        <div className='flex flex-wrap gap-x-8 gap-y-16'>
+          {}
+        </div>
+      </section>
+    </>
   );
 };
 
