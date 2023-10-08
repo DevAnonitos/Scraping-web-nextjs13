48ac637e0aa5eb06d84bea7c246b98a0f0c40a5d
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-10-08 17:32:52 +0700

    update:Add->API-Cron

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -1,7 +1,14 @@
 import { NextResponse } from "next/server";
 
+import { connectToDB } from "@/lib/mongoose"; 
+import Product from "@/lib/models/product.model"; 
+
+
+
 export async function GET(request: Request) {
     try {
+        connectToDB();
+
         
     } catch (error: any) {
         throw new Error(`Fail to get all product ${error.message}`);
