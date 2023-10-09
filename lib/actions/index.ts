6fdae573e12a3922a5a7d77410ce99f2f82138bf
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-10-10 00:17:59 +0700

    update:Add->Server-Action

--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -1,11 +1,20 @@
 "use server";
 
 import { User } from "@/types";
+import { connectToDB } from "../mongoose";
+import { scrapeAmazonProduct } from "../scraper";
 import { revalidatePath } from "next/cache";
+import Product from "../models/product.model";
+import { generateEmailBody, sendEmail } from "../nodemailer";
 
 export async function scrapeAndStoreProduct(productUrl: string) {
+
+    if(!productUrl) {
+        return;
+    }
+
     try {
-        
+        connectToDB();
     } catch (error: any) {
         throw new Error(`Failed to create/update product: ${error.message}`);
     }
