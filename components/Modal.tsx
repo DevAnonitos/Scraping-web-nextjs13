23713ca1e61a95c3b98fc31c0ca6453aa08e0af9
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-10-11 01:57:49 +0700

    update:Add->FormModal

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -35,7 +35,7 @@ const Modal = ({ productId }: Props) => {
                 Track
             </button>
 
-            <Transition appear as={Fragment}>
+            <Transition appear show={isOpen} as={Fragment}>
                 <Dialog 
                     as='div' 
                     onClose={closeModal} 
@@ -101,6 +101,41 @@ const Modal = ({ productId }: Props) => {
                                         Never miss a bargain again with our timely alerts!
                                     </p>
                                 </div>
+
+                                <form 
+                                    className='flex flex-col mt-5'
+                                    onSubmit={handleSubmit}
+                                >
+                                    <label 
+                                        htmlFor="email" 
+                                        className='text-sm font-medium text-gray-700'
+                                    >
+                                        Email address
+                                    </label>
+                                    <div className='dialog-input_container'>
+                                        <Image 
+                                            src="/assets/icons/mail.svg"
+                                            alt='mail'
+                                            width={18}
+                                            height={18}
+                                        />
+
+                                        <input
+                                            required
+                                            type='email'
+                                            id='email'
+                                            value={email}
+                                            onChange={(e) => setEmail(e.target.value)}
+                                            placeholder='Enter your email address...'
+                                            className='dialog-input'
+                                        />
+                                    </div>
+
+                                    <button type='submit' className='dialog-btn'>
+                                        {isSubmitting ? 'Submitting...' : 'Track'}
+                                    </button>
+                                </form>
+
                             </div>
                         </Transition.Child>
                     </div>
