5ae3af13a6f038e7faf186d62176bbcba3afe53f
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-10-12 22:45:02 +0700

    update:Add->getNotifications

--- lib/utils.ts
+++ lib/utils.ts
@@ -87,8 +87,23 @@ export const getAveragePrice = (priceList: PriceHistoryItem[]) => {
     return averagePrice;
 };
 
-export const getEmailNotificationType = () => {
+export const getEmailNotificationType = (
+    scrapedProduct: Product, 
+    currentProduct: Product,
+) => {
+    const lowestPrice = getLowestPrice(currentProduct.priceHistory);
 
+    if (scrapedProduct.currentPrice < lowestPrice) {
+        return Notification.LOWEST_PRICE as keyof typeof Notification;
+    }
+    if (!scrapedProduct.isOutOfStock && currentProduct.isOutOfStock) {
+        return Notification.CHANGE_OF_STOCK as keyof typeof Notification;
+    }
+    if (scrapedProduct.discountRate >= THRESHOLD_PERCENTAGE) {
+        return Notification.THRESHOLD_MET as keyof typeof Notification;
+    }
+
+    return null;
 };
 
 export const formatNumber = (num: Number = 0) => {
