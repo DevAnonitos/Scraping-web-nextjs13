4bcc7af5ed33cc0ca3d028b951627be99d5085f6
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-10-12 22:19:11 +0700

    update:Add->HandleGetPriceValue

--- lib/utils.ts
+++ lib/utils.ts
@@ -57,7 +57,34 @@ export const extractDescription = ($: any) => {
 };
 
 export const getHighestPrice = (priceList: PriceHistoryItem[]) => {
+    let highestPrice = priceList[0];
 
+    for(let i = 0; i < priceList.length; i++){
+        if(priceList[i].price > highestPrice.price){
+            highestPrice = priceList[i];
+        }
+    }
+
+    return highestPrice.price;
+};
+
+export const getLowestPrice = (priceList: PriceHistoryItem[]) => {
+    let lowestPrice = priceList[0];
+
+    for(let i = 0; i <priceList.length; i++){
+        if(priceList[i].price < lowestPrice.price){
+            lowestPrice = priceList[i];
+        }
+    }
+
+    return lowestPrice.price;
+};
+
+export const getAveragePrice = (priceList: PriceHistoryItem[]) => {
+    const sumOfPrices = priceList.reduce((acc, curr) => acc + curr.price, 0);
+    const averagePrice = sumOfPrices / priceList.length || 0;
+
+    return averagePrice;
 };
 
 export const getEmailNotificationType = () => {
