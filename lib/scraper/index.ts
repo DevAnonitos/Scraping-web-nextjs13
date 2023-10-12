3b3a68d63d26456a8befa2837e928e3115fe89d0
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-10-12 22:01:31 +0700

    update:Add->Handle-Func-ScraperAmazonProduct

--- lib/scraper/index.ts
+++ lib/scraper/index.ts
@@ -71,13 +71,26 @@ export async function scrapeAmazonProduct(url: string) {
 
         const data = {
             url,
+            currency: currency || "$",
             title,
+            image: imageUrls[0],
             currentPrice: Number(currentPrice) || Number(originalPrice),
             originalPrice: Number(originalPrice) || Number(originalPrice),
+            priceHistory: [],
+            discountRate: Number(discountRate),
+            category: 'category',
+            reviewsCount: 100,
+            stars: 4.5,
+            isOutOfStock: outOfStock,
+            description,
+            lowestPrice: Number(currentPrice) || Number(originalPrice),
+            highestPrice: Number(originalPrice) || Number(currentPrice),
+            averagePrice: Number(currentPrice) || Number(originalPrice),
         };
 
         console.log(data);
 
+        return data;
     } catch (error: any) {
         console.log(error);
     }
