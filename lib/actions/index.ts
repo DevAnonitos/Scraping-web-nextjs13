17deb854000b412a84fd12e37d93eab0314303da
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-10-13 00:48:42 +0700

    update:Add->Action-server

--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -24,7 +24,89 @@ export async function scrapeAndStoreProduct(productUrl: string) {
 
         console.log(scrapedProduct);
 
+        let product = scrapedProduct;
+
+        const existingProduct = await Product.findOne({ url: scrapedProduct.url, });
+
+        if(existingProduct){
+
+        }
+
+
+        const newProduct = await Product.findOneAndUpdate(
+            { url: scrapedProduct.url, },
+            product,
+            { upsert: true, new: true, },
+        );
+        
+        revalidatePath(`/products/${newProduct._id}`);
     } catch (error: any) {
         throw new Error(`Failed to create/update product: ${error.message}`);
+    }
+};
+
+export async function getProductById(productId: string) {
+    try {
+        connectToDB();
+
+        const product = await Product.findOne({ _id: productId });
+
+        if(!product) return null;
+
+        return product;
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+export async function getAllProducts() {
+    try {
+        connectToDB();
+
+        const products = await Product.find();
+
+        return products;
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+export async function getSimilarProducts(productId: string) {
+    try {
+        connectToDB();
+
+        const currentProduct = await Product.findById(productId);
+
+        if(!currentProduct) return null;
+
+        const similarProducts = await Product.find({
+            _id: { $ne: productId },
+        }).limit(3);
+
+        return similarProducts;
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+export async function addUserEmailToProduct(productId: string, userEmail: string) {
+    try {
+        const product = await Product.findById(productId);
+
+        if(!product) return;
+
+        const userExists = product.users.some((user: User) => user.email === userEmail);
+
+        if(!userExists) {
+            product.users.push({ email: userEmail });
+
+            await product.save();
+
+            const emailContent = await generateEmailBody(product, "WELCOME");
+
+        
+        }
+    } catch (error) {
+        console.log(error);
     }
 };
